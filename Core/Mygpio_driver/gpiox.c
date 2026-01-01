cf4a56726d03597b75c16b3fe78361468de1b314
Bavin <bavinbav5@gmail.com>
2026-01-01 20:28:09 +0300

    removed a lil bug in set reset funcs and added cleaning registers before setting them up and now th elib is good togo

--- Core/Mygpio_driver/gpiox.c
+++ Core/Mygpio_driver/gpiox.c
@@ -10,16 +10,23 @@ static inline void port_init(GPIO_TypeDef *ptr) {
 }
 void gpio_init(GPIO_TypeDef *port, gpio_set_up *ptr) {
 	port_init(port);
+	port->MODER &= ~((0b11) << (ptr->PINx * 2));
 	port->MODER |= ((ptr->MODERx) << (ptr->PINx * 2));
+
+	port->OTYPER &= ~((0b1) << (ptr->PINx));
 	port->OTYPER |= ((ptr->OTYPERx) << (ptr->PINx));
+
+	port->OSPEEDR &= ~((0b11) << (ptr->PINx * 2));
 	port->OSPEEDR |= ((ptr->OSPEEDRx) << (ptr->PINx * 2));
+
+	port->PUPDR &= ~((0b11) << (ptr->PINx * 2));
 	port->PUPDR |= ((ptr->PUPDRx) << (ptr->PINx * 2));
 
 }
 void gpio_set(GPIO_TypeDef *port, uint8_t pin) {
-	port->BSRR |= (1 << pin);
+	port->BSRR = (1 << pin);
 }
 
 void gpio_reset(GPIO_TypeDef *port, uint8_t pin) {
-	port->BSRR |= (1 << (pin + 16));
+	port->BSRR = (1 << (pin + 16));
 }

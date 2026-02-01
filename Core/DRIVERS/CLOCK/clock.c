70c8d62e4c592119444fb290041c6713cd5aee4f
Bavin <bavinbav5@gmail.com>
2026-02-01 19:00:34 +0300

    added flash latency for the clock driver, and also enables DC/IC cache

--- Core/DRIVERS/CLOCK/clock.c
+++ Core/DRIVERS/CLOCK/clock.c
@@ -7,15 +7,28 @@ void clock_init_180Mhz(void){
 	RCC->APB1ENR|=(1<<28);			//since we are gonna run the chip at 180Mhz, it needs more juice i mean voltage
 	PWR->CR|=(3<<14);				//we set the scale mode to 1 max performance
 	while(!(PWR->CSR&(1<<16)));		//wait until Over-drive mode is ready
+	/*
+	 * the CPU will now run at 180Mhz, but we have a problem
+	 * which is the internal perpherials like the flash
+	 * cannot keep up with the speed of the CPU so before we switch to PLL mode we have to
+	 * configure flash wait states and caches
+	 * 0 WS → 0–30 MHz
+	 * 1 WS → 30–60 MHz
+	 * 2 WS → 60–90 MHz
+	 * …
+	 * 5 WS → 168–180 MHz
+	 * WS == wait state
+	 * this is optional but its better to enable flash prefetch
+	 * then we have to enable ID/IC cache bits normally in under 90 Mhz cache is off but for hight speeds
+	 * we have to turn them on
+	 */
+	FLASH->ACR&=~(0b1111);			//clear the flash latency
+	FLASH->ACR|=(5<<0)|(7<<8);		//then we set it to 5 wait state, and enable prefetch, then enable data cache and Instruction cache
 	RCC->PLLCFGR&=~(0b111111);		//clear PLLM bits
 	RCC->PLLCFGR|=(8);				//divide the HSE clock by 8, the target clock after division should be between 1 to 2 Mhz
 	RCC->PLLCFGR&=~(0b111111111);	//clear PLLN bits
 	RCC->PLLCFGR|=(360<<6);			//then multiply the 1 Mhz that we divided in the PLLM to 360, the multiply spot should be between 192Mhz to 432Mhz i want 360 to get 180 easily
 	RCC->PLLCFGR&=~(3<<16);			//set the PLLP bits to 00 which means the PLLN freq will be divided by 2, the 360 that we got we divide by 2 and kaboom we got 180Mhz
-	/*
-	 * the CPU will now run at 180Mhz, but we have a problem
-	 * which is the internal perpherials like the flash
-	 * cannot keep up with the speed of the CPU so before we switch to PLL mode we have to
-	 * configure flash wait states and caches */
+
 
 }
